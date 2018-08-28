17f78a95c0b189635d52b38ce8d0a36bdb7cb09f
cad8697 <michael.molenda@cna.com>
2018-08-28 16:29:47 -0500

    Add post route, remove all logging frameworks

--- src/main/kotlin/com/mikemolenda/requestlog/RequestLogController.kt
+++ src/main/kotlin/com/mikemolenda/requestlog/RequestLogController.kt
@@ -1,17 +1,22 @@
 package com.mikemolenda.requestlog
 
 import org.springframework.http.MediaType
-import org.springframework.web.bind.annotation.RequestMapping
-import org.springframework.web.bind.annotation.RequestMethod
-import org.springframework.web.bind.annotation.RestController
+import org.springframework.web.bind.annotation.*
 
 @RestController
-@RequestMapping("/test")
+@RequestMapping("/")
 class RequestLogController {
-    @RequestMapping(value=["/"],method=[RequestMethod.GET],produces = [MediaType.APPLICATION_JSON_VALUE])
-    fun getDefault(): Response {
-        return Response("OK")
+
+    @RequestMapping(value = ["/test"], method = [RequestMethod.GET], produces = [MediaType.APPLICATION_JSON_VALUE])
+    fun getDefault(@RequestParam(value = "name", required = false) name: String?): Response {
+        return Response("Hello ${name ?: "World"}")
+    }
+
+    @RequestMapping(value = ["/test"], method = [RequestMethod.POST], produces = [MediaType.APPLICATION_JSON_VALUE])
+    fun postDefault(@RequestBody(required = false) request: Request?): Response {
+        return Response("Hello ${request?.name ?: "World"}")
     }
 }
 
+data class Request(val name: String?)
 data class Response(val message: String)
